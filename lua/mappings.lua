4d2e034ccc4209a2cd87438d9e6609b1ef492928
James Vu <jamesvu1998@gmail.com>
2024-12-05 22:00:49 +1100

    updates

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,4 @@
--- local map = vim.api.nvim_set_keymap
+local map = vim.api.nvim_set_keymap
+
+map('n', '<leader>st', ':TermSelect<CR>', { silent = true, noremap = true })
+map('n', '<leader>s,', ':ToggleTermSetName<CR>', { silent = true, noremap = true })
