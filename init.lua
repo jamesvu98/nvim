2a8c72859ebefa793ce25f48d3fc92feb90152d6
James Vu <james.vu@zip.co>
2024-12-06 17:57:08 +1100

    add yaml formatter

--- init.lua
+++ init.lua
@@ -722,6 +722,7 @@ require('lazy').setup({
         python = { 'ruff' },
         go = { 'gofmt' },
         terraform = { 'terraform_fmt' },
+        yaml = { 'prettierd' },
 
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd' },
